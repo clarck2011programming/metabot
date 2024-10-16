5c4e3fb1bd0a5d9bf31aadd5cc073e56ced32a95
clarck2011programming <167099906+clarck2011programming@users.noreply.github.com>
2024-10-16 21:40:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome Code</title>
+  <title>WELCOME</title>
   <link rel="stylesheet" href="assets/files/style1.css">
+  <img src="assets/images/mechalogo.png" alt="favicon">
 </head>
 <body>
   <div class="container">
     <h1>Welcome to My Website!</h1>
-    <img src="assets/images/mechalogo.png" alt="Mechalogo">
     <p>This is a simple welcome message.</p>
   </div>
   <script src="assets/index.js"></script>
