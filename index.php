63b0696aa57f507e3b041148456a2511ddb17a3a
clarck2011programming <167099906+clarck2011programming@users.noreply.github.com>
2024-10-16 21:37:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome Code</title>
+  <link rel="stylesheet" href="assets/files/style1.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Welcome to My Website!</h1>
+    <img src="assets/images/mechalogo.png" alt="Mechalogo">
+    <p>This is a simple welcome message.</p>
+  </div>
+  <script src="assets/index.js"></script>
+</body>
+</html>
